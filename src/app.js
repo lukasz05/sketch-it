cd646990e0a5dd90d75df32c4dbec3e3ca7dc118
Łukasz Orawiec <58004501+lukasz05@users.noreply.github.com>
2022-01-07 18:35:49 +0100

    Test CI/CD (#2)

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 
 app.listen(PORT, HOST);
