88db5eaf76df2670d73ec8dbd16a2b247814cde1
Michał Myczkowski <myczkowskimichal@gmail.com>
2022-01-26 20:59:58 +0100

    room view prototype

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const express = require("express");
-const ejs = require('ejs');
+const ejs = require("ejs");
 const RoomRequestsHandler = require("./rooms/room-requests-handler");
 const path = require("path");
 const { Server } = require("socket.io");
@@ -11,16 +11,21 @@ const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 
-app.set('views', './views');
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile );
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", "./views");
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile );
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
 
 /* GET endpoints (for browsers) */
+
 app.get("/", (req, res) => {
-    res.render('main.html');
+    res.render("main.html");
+});
+
+app.get("/rooms", (req, res) => {
+    res.render("rooms.html");
 });
 
 const io = new Server(server);
