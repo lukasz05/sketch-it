bc03e37f1c07eeae23f4e7f3e0da011eedfbc31b
Łukasz Orawiec <58004501+lukasz05@users.noreply.github.com>
2022-03-23 21:52:06 +0100

    CORS fix

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:8080",
+        origin: "https://jolly-island-0a4fc1003.1.azurestaticapps.net",
     },
 });
 
